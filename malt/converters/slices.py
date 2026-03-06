c3b6c7a695492ff67a47b74c22e95a753cc19e23
David Ittah <dime10@users.noreply.github.com>
2026-03-06 12:15:34 -0500

    Migrate augmented assignment support from Catalyst

--- malt/converters/slices.py
+++ malt/converters/slices.py
@@ -28,6 +28,15 @@ class SliceTransformer(converter.Base):
   Currently, relying on the default slice operator that Tensor uses is
   insufficient, because TensorArray and tensor lists use dedicated index read
   and write functions.
+
+  Also converts array element operator assignment statements into calls to update_item_with_{op},
+  where op is one of the following:
+
+    - `add` corresponding to `+=`
+    - `sub` to `-=`
+    - `mult` to `*=`
+    - `div` to `/=`
+    - `pow` to `**=`
   """
 
   def _process_single_assignment(self, target, value):
@@ -64,6 +73,55 @@ class SliceTransformer(converter.Base):
     if len(node.targets) != 1:
       raise NotImplementedError('multiple assignment')
     replacement = self._process_single_assignment(node.targets[0], node.value)
+    if replacement is not None:
+      return replacement
+    return node
+
+  def _process_single_update(self, target, op, value):
+    if not isinstance(target, ast.Subscript):
+      return None
+    s = target.slice
+    if isinstance(s, (ast.Tuple, ast.Call)):
+      return None
+    if not isinstance(op, (ast.Mult, ast.Add, ast.Sub, ast.Div, ast.Pow)):
+      return None
+
+    template = f"""
+      target = ag__.update_item_with_op(target, index, x, "{type(op).__name__.lower()}")
+    """
+    lower, upper, step = None, None, None
+
+    if isinstance(s, ast.Slice):
+      # Replace unused arguments in template with "None" to preserve each arguments' position.
+      # templates.replace ignores None and does not accept string so change is applied here.
+      lower_str = "lower" if s.lower is not None else "None"
+      upper_str = "upper" if s.upper is not None else "None"
+      step_str = "step" if s.step is not None else "None"
+      template = template.replace("index", f"slice({lower_str}, {upper_str}, {step_str})")
+
+      lower, upper, step = s.lower, s.upper, s.step
+
+    return templates.replace(
+      template,
+      target=target.value,
+      index=target.slice,
+      lower=lower,
+      upper=upper,
+      step=step,
+      x=value,
+    )
+
+  def visit_AugAssign(self, node):
+    """The AugAssign node is replaced with a call to ag__.update_item_with_{op}
+    when its target is a single index array subscript and its op is an arithmetic
+    operator (i.e. Add, Sub, Mult, Div, or Pow), otherwise the node is left as is.
+
+    Example:
+        `x[i] += y` is replaced with `x = ag__.update_item_with(x, i, y)`
+        `x[i] ^= y` remains unchanged
+    """
+    node = self.generic_visit(node)
+    replacement = self._process_single_update(node.target, node.op, node.value)
     if replacement is not None:
       return replacement
     return node
